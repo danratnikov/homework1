2d2a56aac56599c3ec2ef77e6db4dee41e4a67e3
Dan Ratnikov <ratnikov.danil@gmail.com>
2014-12-19 04:10:29 +0500

    Changeable title

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Домашняя работа №1</title>
+    <title><?php echo $title ?></title>
     <link rel="stylesheet" href="fonts/fira/fira.css">
     <link rel="stylesheet" href="fonts/HelveticaNeue/helveticaneue.css">
     <link rel="stylesheet" href="fonts/Proxima-Nova/ProximaNova.css">
--- index.php
+++ index.php
@@ -9,18 +9,22 @@ $page = $_GET['page'];
 
 switch ($page) {
 	case 'admin':
+		$title = "Авторизация";
 		require "admin.php";
 		break;
 	
 	case 'works':
+		$title = "Мои работы";
 		require "works.php";
 		break;
 	
 	case 'contacts':
+		$title = "Мои контакты";
 		require "contacts.php";
 		break;
 	
 	default:
+		$title = "Обо мне";
 		$page = 'about';
 		require "about.php";
 		break;
